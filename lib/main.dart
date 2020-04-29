8f80a1c207db4965d6fb126aaa8a6a74507aab0e
lily <614720105@qq.com>
2020-04-29 12:40:47 +0800

    time:2020.04.29 content:http:package.使用WebSocket.(示例：数据不知道发送是否成功)

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 Future<Post> fetchPost() async {
-  final response =
-      await http.get('https://jsonplaceholder.typicode.com/posts/1',
-      headers: {HttpHeaders.AUTHORIZATION:"Basic your_api_token_here"},//添加认证请求
-      );
+  final response = await http.get(
+    'https://jsonplaceholder.typicode.com/posts/1',
+    headers: {HttpHeaders.AUTHORIZATION: "Basic your_api_token_here"}, //添加认证请求
+  );
   final respenseJson = json.decode(response.body);
   return new Post.fromJson(respenseJson);
 }
@@ -44,15 +46,16 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: WebSocketsPage(),
+//      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget{
+//=============================获取网络数据=======================================//
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Fetch data Example'),
@@ -72,5 +75,69 @@ class MyHomePage extends StatelessWidget{
     );
     throw UnimplementedError();
   }
+}
 
+//=============================使用WebSockets====================================//
+class WebSocketsPage extends StatefulWidget {
+  final String title = "Web Sockets";
+  final WebSocketChannel channel =
+      new IOWebSocketChannel.connect('ws://echo.websocket.org');
+  @override
+  State<StatefulWidget> createState() {
+    return new _WebSocketsPageState();
+    throw UnimplementedError();
+  }
+}
+
+class _WebSocketsPageState extends State<WebSocketsPage> {
+  TextEditingController _controller = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: new Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            new Form(
+              child: new TextFormField(
+                controller: _controller,
+                decoration: new InputDecoration(labelText: 'Send a message'),
+              ),
+            ),
+            new StreamBuilder(
+              stream: widget.channel.stream,
+              builder: (context, snapshot) {
+                return new Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 24.0),
+                  child: new Text(snapshot.hasData ? '${snapshot.data}' : ''),
+                );
+              },
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _sendMessage,
+        tooltip: 'send message',
+        child: new Icon(Icons.send),
+      ),
+    );
+    throw UnimplementedError();
+  }
+
+  void _sendMessage() {
+    if (_controller.text.isNotEmpty) {
+      widget.channel.sink.add(_controller.text);
+    }
+  }
+
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+    super.dispose();
+  }
 }
